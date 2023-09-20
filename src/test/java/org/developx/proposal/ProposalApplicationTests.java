3c9cccf4d70b099927d44195f6af115894ffe31d
kimtaehan11 <kimtaehan11@gmail.com>
2023-09-20 10:34:05 +0900

    first commit

---
+++ src/test/java/org/developx/proposal/ProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.developx.proposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
